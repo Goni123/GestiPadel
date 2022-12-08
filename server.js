d354d2970a3936cdfff2a315eeb84f512901ca43
Gonçalo Aguiar <2050219@student.uma.pt>
2022-12-08 11:01:33 +0000

    alteração da rota da lista de torneios

--- server.js
+++ server.js
@@ -62,11 +62,6 @@ app.get('/admin', (req, res) => {
     console.log(req.session.user)
 })
 
-app.post('/listatorneiosadmin', (req, res) => {
-    res.render("Torneios_admin");
-    console.log(req.session.user)
-})
-
 dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 8080
 
